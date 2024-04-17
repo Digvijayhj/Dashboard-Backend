e54571dfad839f453f24af221b8887bc00f72269
Digvijay <50370704+Digvijayhj@users.noreply.github.com>
2024-04-17 13:07:32 -0700

    Initial Commit

---
+++ src/main/java/com/lsu/dashboard/GameRoomDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.lsu.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRoomDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRoomDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsu/dashboard/GameRoomDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsu.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRoomDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
